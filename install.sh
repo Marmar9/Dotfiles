18ad625c1fc1ee35618bdc11202b263214ef8310
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-03-31 20:44:49 +0000

    Got tmux pluggin manager working ( :

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-sudo pacman -S hyprland, pipewire pipewire-pulse pipewire-jack, pavucontrol, xdg-utils, alacritty, openssh, tmux, starship, rsync, firefox, less, neovim, unzip
+sudo pacman -S hyprland, pipewire pipewire-pulse pipewire-jack, pavucontrol, xdg-utils, alacritty, openssh, tmux, starship, rsync, firefox, less, neovim, unzip, wl-clipboard
 
+# Tmux pluggin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
