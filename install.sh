dd560b8ec1a4008149627ac1c265522cf421e358
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-04-02 10:19:42 +0000

    Added yay install script + fixed scale

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
-sudo pacman -S hyprland, pipewire pipewire-pulse pipewire-jack, pavucontrol, xdg-utils, alacritty, openssh, tmux, starship, rsync, firefox, less, neovim, unzip, wl-clipboard
+sudo pacman -S hyprland, pipewire pipewire-pulse pipewire-jack, pavucontrol, xdg-utils, alacritty, openssh, tmux, starship, rsync, firefox, less, neovim, unzip, wl-clipboard, zsh
 
-# Tmux pluggin manager
+#---------------Tmux pluggin manager---------------
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#-----------------------Yay------------------------
+pushd /home/$(whoami)/
+git clone https://aur.archlinux.org/yay.git
+makepkg -si
+popd
+
+#------------------Other packages------------------
+yay -S visual-studio-code-bin
