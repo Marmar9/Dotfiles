aa03677bf4f6d9770882a0c2ebea16e6f6f3755c
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-05-05 12:17:41 +0000

    Added autoclosing brackets and treesiter context

---
+++ .config/nvim/lua/marcin/plugins/nvim-treesitter.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = { "c", "lua", "cpp", "typescript", "javascript", "vim" },
+            sync_install = false,
+            highlight = {
+                enable = true,
+                disable = { "" },
+                additional_vim_regex_highlighting = false
+            },
+        }
+    end
+}
