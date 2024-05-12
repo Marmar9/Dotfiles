6a94187bc8d9952ec76caf30c918046414fae89d
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-05-12 13:33:55 +0000

    nvim: added move line up down keybind

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,17 +10,23 @@ vim.opt.shiftwidth = 4
 
 -- Use spaces instead of tabs
 vim.opt.expandtab = true
-
 vim.o.clipboard = "unnamedplus"
 
--- For line numbers
+-- Line number configuration - Use relative
 vim.o.relativenumber = true
 vim.opt.number = true
-
 vim.opt.commentstring = ""
 
 -- Black hole register mappings
-
 -- Define a keybind prefix for the black hole register
 vim.api.nvim_set_keymap('n', '<leader>d', '"_d', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>d', '"_d', { noremap = true })
+
+
+
+-- Define mappings for moving line up or down
+-- in normal and visual mode currently: <space> + (j for down k for up)
+vim.api.nvim_set_keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>j', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>k', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
