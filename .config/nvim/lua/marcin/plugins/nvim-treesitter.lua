3f622112c3481aaaac26084a742d47ee04f02e6f
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-08-15 09:17:54 +0000

    neovim: Added hcl and terraform treesitter parsers

--- .config/nvim/lua/marcin/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/marcin/plugins/nvim-treesitter.lua
@@ -1,14 +1,14 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "cpp", "typescript", "javascript", "vim", "go", "yaml" },
-			sync_install = false,
-			highlight = {
-				enable = true,
-				disable = { "" },
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "lua", "cpp", "typescript", "javascript", "vim", "go", "yaml", "terraform", "hcl" },
+            sync_install = false,
+            highlight = {
+                enable = true,
+                disable = { "" },
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end,
 }
