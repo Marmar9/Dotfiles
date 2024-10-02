5a519072e2bd87a9439b7b144fe52126a9c51b5c
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-10-02 13:46:15 +0000

    Nvim: lsp message display in window

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 require("marcin.lazy")
 
 vim.o.termguicolors = true
-vim.cmd'colorscheme catppuccin'
+vim.cmd("colorscheme catppuccin")
 
 -- Set indentation level to 4 spaces
 vim.opt.shiftwidth = 4
@@ -19,14 +19,20 @@ vim.opt.commentstring = ""
 
 -- Black hole register mappings
 -- Define a keybind prefix for the black hole register
-vim.api.nvim_set_keymap('n', '<leader>d', '"_d', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>d', '"_d', { noremap = true })
-
-
+vim.api.nvim_set_keymap("n", "<leader>d", '"_d', { noremap = true })
+vim.api.nvim_set_keymap("v", "<leader>d", '"_d', { noremap = true })
 
 -- Define mappings for moving line up or down
 -- in normal and visual mode currently: <space> + (j for down k for up)
-vim.api.nvim_set_keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>j', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>k', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>j", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>k", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>j", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>k", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+vim.diagnostic.config({
+	virtual_text = false,
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
