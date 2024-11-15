903982119446a03604036a30c4d2c6602f66e509
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2024-11-15 17:42:14 +0000

    Added screenshots

--- configuration_files.sh
+++ configuration_files.sh
@@ -11,3 +11,6 @@ sudo ln -sf /usr/bin/nvim /usr/bin/vi
 mkdir /home/marcin/.local/share/fonts/ 
 (cd /home/marcin/.local/share/fonts/ && sudo curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Lilex.zip && sudo unzip Lilex.zip && sudo rm README.md Lilex.zip LICENSE.txt )
 
+
+# Directory for screenshots
+mkdir /home/${USER}/screenshots
