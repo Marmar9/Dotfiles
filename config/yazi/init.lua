0c389e48b1439224170ed619f092f83e7a2a1208
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2026-01-25 11:35:41 +0100

    feat(yazi): update to latest version

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -2,5 +2,4 @@ require("session"):setup({
 	sync_yanked = true,
 })
 
--- ~/.config/yazi/init.lua
-require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+require("git"):setup()
